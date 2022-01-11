81c6d55607f591ed58a865ffdacbc62007119aaf
Bram Jager <bramjager@upcmail.nl>
2022-01-11 15:40:37 +0100

    Changed Connectionstring to app.config

--- PhoneShop.Business/DataContext.cs
+++ PhoneShop.Business/DataContext.cs
@@ -12,7 +12,7 @@ namespace PhoneShop.Business
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = PhoneshopDBDeel2; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["PhoneshopDB"].ConnectionString);
         }
     }
 }
